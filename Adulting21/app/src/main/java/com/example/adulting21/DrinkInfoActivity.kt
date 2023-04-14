576c37a2c7e4165baa0712fa7f6defd1b831717e
Szizzle2 <gonzalezsayan9131@gmail.com>
2023-04-13 22:36:43 -0700

    Sprint 1 Demo
    First Prototype for sprint 1

---
+++ Adulting21/app/src/main/java/com/example/adulting21/DrinkInfoActivity.kt
@@ -0,0 +1,19 @@
+package com.example.adulting21
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DrinkInfoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.drink_info)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
--- Adulting21/app/src/main/java/com/example/adulting21/MainActivity.kt
+++ Adulting21/app/src/main/java/com/example/adulting21/MainActivity.kt
@@ -1,23 +1,27 @@
 package com.example.adulting21
 
-
+import com.example.adulting21.R
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import android.widget.TextView
-
+import android.widget.Button
+import android.content.Intent
+import android.view.View
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< Updated upstream
         setContentView(R.layout.activity_main)
-=======
-        setContentView(R.layout.drink_info)
->>>>>>> Stashed changes
+
+        //val button3 = findViewById<Button>(R.id.button3)
+        val button3 = findViewById<ImageButton>(R.id.imageView5)
+        button3.setOnClickListener {
+            val intent = Intent(this, DrinkInfoActivity::class.java)
+            startActivity(intent)
+        }
+        //val button3: Button = findViewById(R.id.button3)
+       //setContentView(R.layout.drink_info)
 /*
             GlobalScope.launch(Dispatchers.IO) {
                 val apiService = CocktailApiService()
@@ -32,4 +36,5 @@ class MainActivity : AppCompatActivity() {
 
  */
         }
+
     }
