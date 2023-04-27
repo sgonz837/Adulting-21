2922175a01d7afcf539f60973b422b800bce4a10
jagordon66 <jacobagordon@icloud.com>
2023-04-27 11:23:16 -0400

    Webpage CSS Update
    Updated the webpage with more CSS styling and the webpage has a background image.

--- Webpage/A21DL.php
+++ Webpage/A21DL.php
@@ -13,14 +13,24 @@
 -->
 
 <html lang="en">
-	<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
 	<head>
-		<h1>Adulting21</h1>
+		<div class="shape"><h1>Adulting21</h1></div>
+		<br>
 	</head>
 
-	<body>
-		<center><img src="images/logo.png" alt="Adulting21 Logo"></center>
-		<p style="text-align:center; font-size:24px;">This app is designed to help first-time drinkers get started with drinking.</p>
-		<p style="text-align:center; font-size:24px;"><a href="https://www.kutztown.edu/">University Website</a></p>
+	<body background="https://imgcp.aacdn.jp/img-a/1200/900/global-aaj-front/article/2016/02/56b9a4f563c99_56b9a4c99915d_1461357925.jpg">
+		<div class="img">
+			<img src="images/logo.png" alt="Adulting21 Logo" width="160" height="163">
+		</div>
+		<div class="paragraph">
+			<p>Adulting21 allows young adults to remain safe when entering the world of drinking. </p>
+			<p>Our app provides ingredient, calorie, and alcohol content on the provided drinks.</p> 
+			<p>The app also gives information to local bars in the Kutztown area and allows users to provide reviews on all drinks consumed.</p>
+			<p> We also provide a feature that shows the user their BAC and we have a safety feature. </p>
+			<p>The goal of our app is to provide the user with relevant information pertaining to entering the drinking scene. </p>
+			<p>We want all young adults to drink responsibly!</p><br>
+			<p class="link"><a href="adulting21.eastus.cloudapp.azure.com">Adulting21 Download Link</a></p>
+		</div>
 	</body>
 </html>
