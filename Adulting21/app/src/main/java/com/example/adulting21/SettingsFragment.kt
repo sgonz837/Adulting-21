93ebfa8314a09cbe8c3de720ae3576a0d3a3a42e
Szizzle2 <gonzalezsayan9131@gmail.com>
2023-10-15 15:44:52 -0700

    Profile Submission Complete

--- Adulting21/app/src/main/java/com/example/adulting21/SettingsFragment.kt
+++ Adulting21/app/src/main/java/com/example/adulting21/SettingsFragment.kt
@@ -2,43 +2,43 @@ package com.example.adulting21
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 
-//import com.github.anastr.speedviewlib.SpeedometerView // Uncomment this import
-//Code has been pulled from https://github.com/codeplayon/AndroidSpeedometer
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SettingsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    //Here are the variables names for the calculator
+    private var username: String? = null
+    private var age: Int? = null
+    private var weight: Int? = null
+    private var sex: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
+        // Retrieve user data from shared preference
+        val sharedPref = requireActivity().getSharedPreferences("user_data", AppCompatActivity.MODE_PRIVATE)
+        username = sharedPref.getString("username", "DefaultUsername")
+        age = sharedPref.getInt("age", 0)
+        weight = sharedPref.getInt("weight", 0)
+        sex = sharedPref.getString("sex", "DefaultSex")
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        Log.d("TAG", "Username: $username, Age: $age, Weight: $weight, Sex: $sex")
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -67,22 +67,16 @@ class SettingsFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(username: String, age: Int, weight: Int, sex: String) =
             SettingsFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString("username", username)
+                    putInt("age", age)
+                    putInt("weight", weight)
+                    putString("sex", sex)
                 }
             }
     }
+
 }
